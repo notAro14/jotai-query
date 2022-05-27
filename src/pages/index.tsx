fd74ae04d5d33cbc06f6c953d3fffed794f32f5c
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-05-28 00:45:27 +0200

    Test jotai/query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 // PACKAGES
 import Head from "next/head"
+import { atom, useAtom } from "jotai"
+import { atomWithQuery } from "jotai/query"
 // TYPES & CONSTANTS
 import type { NextPageWithLayout } from "src/types"
 // COMPONENTS
-import Hello from "src/components/hello"
 import Heading from "src/shared/heading"
+import Text from "src/shared/text"
 
+const idAtom = atom(1)
+const userAtom = atomWithQuery((get) => ({
+  queryKey: ["users", get(idAtom)],
+  queryFn: async ({ queryKey: [, id] }) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    return res.json()
+  },
+}))
+
+const User = () => {
+  const [data] = useAtom(userAtom)
+  return <Text>{data.name}</Text>
+}
 const IndexPage: NextPageWithLayout = () => {
   return (
     <>
@@ -17,7 +32,7 @@ const IndexPage: NextPageWithLayout = () => {
         <Heading as="h1" css={{ mb: "$base" }}>
           Next Typescript Starter
         </Heading>
-        <Hello />
+        <User />
       </main>
     </>
   )
--- src/shared/layouts.ts
+++ src/shared/layouts.ts
@@ -3,6 +3,6 @@ import { styled } from "src/styles/stitches.config"
 
 export const Global = styled("div", {
   backgroundColor: "$bg-app",
-  height: "100%",
+  minHeight: "100%",
   p: "$base",
 })
